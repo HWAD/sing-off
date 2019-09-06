f8606895579263c3cb312b5da3838501586c885f
Ryohei Watanabe <watanaber17@gmail.com>
2019-09-06 12:05:16 +0900

    runner and build data

--- ios/Runner/AppDelegate.h
+++ ios/Runner/AppDelegate.h
@@ -1,5 +1,19 @@
-#import <Flutter/Flutter.h>
-#import <UIKit/UIKit.h>
+import <Flutter/Flutter.h>
+import <UIKit/UIKit.h>
+import Firebase
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions:
+                     [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+}
 
 @interface AppDelegate : FlutterAppDelegate
 
