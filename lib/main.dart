67355f386556a79b23168ea43ce1e174582c8204
Ryohei Watanabe <watanaber17@gmail.com>
2019-09-10 18:40:48 +0900

    for Takashi one commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 import './manager.dart';
+import './_upload.dart';
+import './_filepicker.dart';
 
 void main() => runApp(App());
 
@@ -9,15 +11,15 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        brightness: Brightness.light,
-        primarySwatch: Colors.deepOrange,
-        accentColor: Colors.deepPurple
-      ),
+          brightness: Brightness.light,
+          primarySwatch: Colors.deepOrange,
+          accentColor: Colors.deepPurple),
       home: Scaffold(
         appBar: AppBar(
           title: Text('Karaoke Mania'),
         ),
-        body: Manager(startingMenu: 'Start Value'),
+        // body: Manager(startingMenu: 'Start Value'),
+        body: FilePickerDemo(),
       ),
     );
   }
